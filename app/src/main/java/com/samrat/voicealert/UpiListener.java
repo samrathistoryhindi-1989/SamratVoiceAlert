12bdee37e5a94a969fb4bcd969654f4920ff4258
samrathistoryhindi-1989 <samrathistoryhindi-1989@users.noreply.github.com>
2026-09-16 23:14:57 +0530

    upi 10rs voice fix

---
+++ app/src/main/java/com/samrat/voicealert/UpiListener.java
@@ -0,0 +1,19 @@
+package com.samrat.voicealert;
+import android.service.notification.*; import android.os.Bundle;
+public class UpiListener extends NotificationListenerService {
+@Override public void onNotificationPosted(StatusBarNotification sbn){
+String pkg=sbn.getPackageName();
+if(pkg.contains("phonepe")||pkg.contains("tez")||pkg.contains("paytm")||pkg.contains("upi")||pkg.contains("paisa")){
+try{
+Bundle e=sbn.getNotification().extras;
+String txt=(e.getString("android.title","")+" "+e.getString("android.text","")).toLowerCase();
+double amt=MainActivity.extractAmount(txt);
+if(amt>0 && MainActivity.instance!=null){
+boolean cr = txt.contains("received")||txt.contains("credit")||txt.contains("credited")||txt.contains("vachayi")||txt.contains("received money");
+String vpa="UPI";
+MainActivity.instance.runOnUiThread(()->MainActivity.instance.showAlert(amt,cr,vpa));
+}
+}catch(Exception ex){}
+}
+}
+}
